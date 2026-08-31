dcd77e0d2257080f94131aa81d3837b6dc32a979
Seth Mueller <seth.m.mueller@gmail.com>
2026-08-31 14:08:49 -0500

    start work on radio control

--- main.py
+++ main.py
@@ -1,76 +1,28 @@
-"""
-import numpy as np
-import adi
-
-sample_rate = 1e6 # Hz
-center_freq = 915e6 # Hz
-num_samps = 10000 # number of samples returned per call to rx()
-
-sdr = adi.Pluto('ip:192.168.2.1')
-sdr.gain_control_mode_chan0 = 'manual'
-sdr.rx_hardwaregain_chan0 = 70.0 # dB
-sdr.rx_lo = int(center_freq)
-sdr.sample_rate = int(sample_rate)
-sdr.rx_rf_bandwidth = int(sample_rate) # filter width, just set it to the same as sample rate for now
-sdr.rx_buffer_size = num_samps
-
-samples = sdr.rx() # receive samples off Pluto
-print(samples[0:10])
-"""
 import numpy as np
 import adi
 import matplotlib.pyplot as plt
 from gui import *
 
-a = 0
-def cool_plot():
-    global a
-    # Create matplotlib figure
-    fig, ax = plt.subplots(figsize=(3, 3))
-
-    ax.plot([1,2,3], [4,3,2])
-    ax.set_title("Plot! " + str(a))
-    a += 1
-    ax.grid(True)
-    
-    return fig
-
-gui = GUI()
-
-gui.rx_controls.live_rx_callback         = lambda checked : print("live rx: " + str(checked))
-gui.rx_controls.single_rx_callback       = lambda : print("single rx")
-gui.rx_controls.set_manaul_gain_callback = lambda val_db : print("set manual rx gain: " + val_db)
-gui.rx_controls.set_auto_gain_callback   = lambda agc_setting : print("agc setting: " + agc_setting)
-gui.rx_controls.probe_usb_callback       = lambda : print("probe usb")
-
-gui.tx_controls.tx_enable_callback     = lambda checked : print("tx enable: " + str(checked))
-gui.tx_controls.set_gain_callback      = lambda val : print("set tx gain: " + val)
-gui.tx_controls.test_cw_check_callback = lambda checked : print("test cw: " + str(checked))
-gui.tx_controls.set_cw_offset_callback = lambda freq : print("set cw offset: " + str(freq))
-
-root.mainloop()
-
-
-"""
 sample_rate = 1e6 # Hz
 center_freq = 915e6 # Hz
-num_samps = 100000 # number of samples per call to rx()
+num_samps = 10000 # number of samples per call to rx()
 
 sdr = adi.Pluto("ip:192.168.2.1")
 sdr.sample_rate = int(sample_rate)
 
 # Config Tx
-sdr.tx_rf_bandwidth = int(sample_rate) # filter cutoff, just set it to the same as sample rate
+sdr.tx_rf_bandwidth = int(sample_rate) # filter cutoff, same as sample rate
 sdr.tx_lo = int(center_freq)
-sdr.tx_hardwaregain_chan0 = -50 # Increase to increase tx power, valid range is -90 to 0 dB
+sdr.tx_hardwaregain_chan0 = -50 # -90 to 0 dB
 
 # Config Rx
 sdr.rx_lo = int(center_freq)
 sdr.rx_rf_bandwidth = int(sample_rate)
 sdr.rx_buffer_size = num_samps
 sdr.gain_control_mode_chan0 = 'manual'
-sdr.rx_hardwaregain_chan0 = 20 # dB, increase to increase the receive gain, but be careful not to saturate the ADC
+sdr.rx_hardwaregain_chan0 = 50 # 0 - 70
 
+"""
 # Create transmit waveform (QPSK, 16 samples per symbol)
 num_symbols = 1000
 x_int = np.random.randint(0, 4, num_symbols) # 0 to 3
@@ -113,3 +65,47 @@ plt.xlabel("Frequency [MHz]")
 plt.ylabel("PSD")
 plt.show()
 """
+
+def test_cw_callback(checked):
+    if checked:
+        print("testing CW")
+        N = 10000
+        t = np.arange(N)/sample_rate
+        samples = 0.5*np.exp(2.0j*np.pi*100e3*t)
+        sameples *= 2**14
+
+        sdr.tx_cyclic_buffer = True # Enable cyclic buffers
+        sdr.tx(samples) # start transmitting
+    else:
+        print("stopping CW")
+        sdr.tx_destroy_buffer()
+
+a = 0
+def rx_plot():
+    global a
+    samples = sdr.rx()
+
+    # Create matplotlib figure
+    fig, ax = plt.subplots(figsize=(3, 3))
+
+    ax.plot(np.arange(len(samples)), samples.real)
+    ax.set_title("Plot! " + str(a))
+    a += 1
+    ax.grid(True)
+    
+    return fig
+
+gui = GUI()
+
+gui.rx_controls.live_rx_callback         = lambda checked : print("live rx: " + str(checked))
+gui.rx_controls.single_rx_callback       = lambda : gui.rx_graphs.add_plot(rx_plot())
+gui.rx_controls.set_manaul_gain_callback = lambda val_db : print("set manual rx gain: " + val_db)
+gui.rx_controls.set_auto_gain_callback   = lambda agc_setting : print("agc setting: " + agc_setting)
+gui.rx_controls.probe_usb_callback       = lambda : print("probe usb")
+
+gui.tx_controls.tx_enable_callback     = lambda checked : print("tx enable: " + str(checked))
+gui.tx_controls.set_gain_callback      = lambda val : print("set tx gain: " + val)
+gui.tx_controls.test_cw_check_callback = test_cw_callback
+gui.tx_controls.set_cw_offset_callback = lambda freq : print("set cw offset: " + str(freq))
+
+root.mainloop()
